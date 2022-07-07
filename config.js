cc4ddee2d603085cf07c4764a582b7c819b7f9b8
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-07 19:05:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
-  ['0'],
-  ['6281224863098', 'Della', true]
+  ['6281322577123'],
+  ['6281322577123'],
+  ['6281322577123', 'Lintang', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'LintangBotz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
